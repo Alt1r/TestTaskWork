2a6d7b347ddad6877a976cb9c02a4db65abe6665
Elnar Makhmudov <altir_work@mail.ru>
2019-10-25 08:28:59 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace TestTaskLibrary
+{
+    public interface IHeatingModel
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        double Temperature { get; set; }
+    }
+
+    public class ModelHeatingPanel : IHeatingModel
+    {
+        public int Id { get; set; }
+
+        int IsEntryAutomateOn { get; set; }
+        int IsNetworkOn { get; set; }
+        int IsPowerOn { get; set; }
+        int IsPoIsOnUpswerOn { get; set; }
+
+        public string Name { get; set; }
+        public double Temperature { get; set; }
+
+        public enum Location : int
+        {
+            X,
+            Y
+        };
+
+        public Boolean IsInAlarm { get; set; }
+        
+        
+
+    }
+}
---
+++ TestTaskFromWork/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestTaskFromWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
