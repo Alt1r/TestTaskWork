32db727dc192415c1dff24846ce77c74a3d05c01
Elnar Makhmudov <altir_work@mail.ru>
2019-10-28 01:07:09 +0500

    Попытка реализовать классы для отображения элементов

--- WindowTestTask/Form1.Designer.cs
+++ WindowTestTask/Form1.Designer.cs
@@ -31,12 +31,19 @@
             this.diagramControl1 = new DevExpress.XtraDiagram.DiagramControl();
             this.diagramShape1 = new DevExpress.XtraDiagram.DiagramShape();
             this.diagramShape2 = new DevExpress.XtraDiagram.DiagramShape();
+            this.diagramContainer1 = new DevExpress.XtraDiagram.DiagramContainer();
             this.diagramShape3 = new DevExpress.XtraDiagram.DiagramShape();
             this.diagramShape4 = new DevExpress.XtraDiagram.DiagramShape();
             this.diagramShape5 = new DevExpress.XtraDiagram.DiagramShape();
             this.diagramShape6 = new DevExpress.XtraDiagram.DiagramShape();
             this.diagramShape7 = new DevExpress.XtraDiagram.DiagramShape();
             this.diagramShape8 = new DevExpress.XtraDiagram.DiagramShape();
+            this.diagramShape9 = new DevExpress.XtraDiagram.DiagramShape();
+            this.diagramShape10 = new DevExpress.XtraDiagram.DiagramShape();
+            this.diagramShape11 = new DevExpress.XtraDiagram.DiagramShape();
+            this.diagramShape12 = new DevExpress.XtraDiagram.DiagramShape();
+            this.diagramShape13 = new DevExpress.XtraDiagram.DiagramShape();
+            this.diagramShape14 = new DevExpress.XtraDiagram.DiagramShape();
             ((System.ComponentModel.ISupportInitialize)(this.diagramControl1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,12 +52,8 @@
             this.diagramControl1.Items.AddRange(new DevExpress.XtraDiagram.DiagramItem[] {
             this.diagramShape1,
             this.diagramShape2,
-            this.diagramShape3,
-            this.diagramShape4,
-            this.diagramShape5,
-            this.diagramShape6,
-            this.diagramShape7,
-            this.diagramShape8});
+            this.diagramContainer1,
+            this.diagramShape14});
             this.diagramControl1.Location = new System.Drawing.Point(12, 12);
             this.diagramControl1.Name = "diagramControl1";
             this.diagramControl1.OptionsBehavior.SelectedStencils = new DevExpress.Diagram.Core.StencilCollection(new string[] {
@@ -68,68 +71,138 @@
             this.diagramShape1.Content = "12";
             this.diagramShape1.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
             this.diagramShape1.Position = new DevExpress.Utils.PointFloat(140F, 120F);
-            this.diagramShape1.Size = new System.Drawing.SizeF(100F, 75F);
+            this.diagramShape1.Size = new System.Drawing.SizeF(80F, 45F);
             this.diagramShape1.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
             // 
             // diagramShape2
             // 
-            this.diagramShape2.BackgroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1_4;
-            this.diagramShape2.Content = "";
+            this.diagramShape2.BackgroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black_1;
+            this.diagramShape2.Content = "22";
             this.diagramShape2.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
-            this.diagramShape2.Position = new DevExpress.Utils.PointFloat(370F, 120F);
+            this.diagramShape2.Position = new DevExpress.Utils.PointFloat(140F, 260F);
             this.diagramShape2.Shape = DevExpress.Diagram.Core.BasicShapes.RoundedRectangle;
-            this.diagramShape2.Size = new System.Drawing.SizeF(130F, 182F);
+            this.diagramShape2.Size = new System.Drawing.SizeF(100F, 55F);
             this.diagramShape2.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            // 
+            // diagramContainer1
+            // 
+            this.diagramContainer1.BackgroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
+            this.diagramContainer1.Header = "Heading";
+            this.diagramContainer1.Items.AddRange(new DevExpress.XtraDiagram.DiagramItem[] {
+            this.diagramShape3,
+            this.diagramShape4,
+            this.diagramShape5,
+            this.diagramShape6,
+            this.diagramShape7,
+            this.diagramShape8,
+            this.diagramShape9,
+            this.diagramShape10,
+            this.diagramShape11,
+            this.diagramShape12,
+            this.diagramShape13});
+            this.diagramContainer1.Position = new DevExpress.Utils.PointFloat(310F, 120F);
+            this.diagramContainer1.Shape = DevExpress.Diagram.Core.StandardContainers.Alternating;
+            this.diagramContainer1.Size = new System.Drawing.SizeF(130F, 188F);
+            this.diagramContainer1.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
             // 
             // diagramShape3
             // 
-            this.diagramShape3.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.diagramShape3.Content = "Авария";
+            this.diagramShape3.Appearance.Font = new System.Drawing.Font("Tahoma", 18F);
+            this.diagramShape3.Content = "ЩУЭ-1";
             this.diagramShape3.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
-            this.diagramShape3.Position = new DevExpress.Utils.PointFloat(385F, 236F);
-            this.diagramShape3.Size = new System.Drawing.SizeF(100F, 55F);
-            this.diagramShape3.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
-            this.diagramShape3.ThemeStyleId = DevExpress.Diagram.Core.DiagramShapeStyleId.Variant3;
+            this.diagramShape3.Position = new DevExpress.Utils.PointFloat(20F, 8F);
+            this.diagramShape3.Size = new System.Drawing.SizeF(100F, 15F);
+            this.diagramShape3.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
             // 
             // diagramShape4
             // 
-            this.diagramShape4.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
-            this.diagramShape4.Content = "22";
+            this.diagramShape4.Content = "Вв. автомат";
             this.diagramShape4.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
-            this.diagramShape4.Position = new DevExpress.Utils.PointFloat(140F, 260F);
-            this.diagramShape4.Shape = DevExpress.Diagram.Core.BasicShapes.RoundedRectangle;
-            this.diagramShape4.Size = new System.Drawing.SizeF(100F, 55F);
-            this.diagramShape4.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            this.diagramShape4.Position = new DevExpress.Utils.PointFloat(8F, 38F);
+            this.diagramShape4.Size = new System.Drawing.SizeF(100F, 15F);
+            this.diagramShape4.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
             // 
             // diagramShape5
             // 
-            this.diagramShape5.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.diagramShape5.Position = new DevExpress.Utils.PointFloat(470F, 160F);
-            this.diagramShape5.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
-            this.diagramShape5.Size = new System.Drawing.SizeF(15F, 15F);
+            this.diagramShape5.Content = "Связь";
+            this.diagramShape5.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            this.diagramShape5.Position = new DevExpress.Utils.PointFloat(8F, 54F);
+            this.diagramShape5.Size = new System.Drawing.SizeF(100F, 15F);
+            this.diagramShape5.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
             // 
             // diagramShape6
             // 
-            this.diagramShape6.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.diagramShape6.Position = new DevExpress.Utils.PointFloat(470F, 176F);
-            this.diagramShape6.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
-            this.diagramShape6.Size = new System.Drawing.SizeF(15F, 15F);
+            this.diagramShape6.Content = "Питание";
+            this.diagramShape6.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            this.diagramShape6.Position = new DevExpress.Utils.PointFloat(8F, 70F);
+            this.diagramShape6.Size = new System.Drawing.SizeF(100F, 15F);
+            this.diagramShape6.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
             // 
             // diagramShape7
             // 
-            this.diagramShape7.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.diagramShape7.Position = new DevExpress.Utils.PointFloat(470F, 192F);
-            this.diagramShape7.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
-            this.diagramShape7.Size = new System.Drawing.SizeF(15F, 15F);
+            this.diagramShape7.Content = "На ИБП";
+            this.diagramShape7.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            this.diagramShape7.Position = new DevExpress.Utils.PointFloat(8F, 86F);
+            this.diagramShape7.Size = new System.Drawing.SizeF(100F, 15F);
+            this.diagramShape7.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
             // 
             // diagramShape8
             // 
-            this.diagramShape8.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.diagramShape8.Content = "";
+            this.diagramShape8.Content = "Темп. 12С";
             this.diagramShape8.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
-            this.diagramShape8.Position = new DevExpress.Utils.PointFloat(470F, 208F);
-            this.diagramShape8.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
-            this.diagramShape8.Size = new System.Drawing.SizeF(15F, 15F);
+            this.diagramShape8.Position = new DevExpress.Utils.PointFloat(8F, 102F);
+            this.diagramShape8.Size = new System.Drawing.SizeF(100F, 15F);
+            this.diagramShape8.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
+            // 
+            // diagramShape9
+            // 
+            this.diagramShape9.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.diagramShape9.Content = "Авария";
+            this.diagramShape9.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            this.diagramShape9.Position = new DevExpress.Utils.PointFloat(30F, 128F);
+            this.diagramShape9.Size = new System.Drawing.SizeF(80F, 45F);
+            this.diagramShape9.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            // 
+            // diagramShape10
+            // 
+            this.diagramShape10.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.diagramShape10.Position = new DevExpress.Utils.PointFloat(110F, 38F);
+            this.diagramShape10.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
+            this.diagramShape10.Size = new System.Drawing.SizeF(16F, 15F);
+            this.diagramShape10.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            // 
+            // diagramShape11
+            // 
+            this.diagramShape11.BackgroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black_1;
+            this.diagramShape11.Position = new DevExpress.Utils.PointFloat(110F, 54F);
+            this.diagramShape11.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
+            this.diagramShape11.Size = new System.Drawing.SizeF(16F, 15F);
+            this.diagramShape11.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            // 
+            // diagramShape12
+            // 
+            this.diagramShape12.BackgroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent2;
+            this.diagramShape12.Position = new DevExpress.Utils.PointFloat(110F, 70F);
+            this.diagramShape12.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
+            this.diagramShape12.Size = new System.Drawing.SizeF(16F, 15F);
+            this.diagramShape12.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            // 
+            // diagramShape13
+            // 
+            this.diagramShape13.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.diagramShape13.Position = new DevExpress.Utils.PointFloat(110F, 86F);
+            this.diagramShape13.Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
+            this.diagramShape13.Size = new System.Drawing.SizeF(16F, 15F);
+            this.diagramShape13.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            // 
+            // diagramShape14
+            // 
+            this.diagramShape14.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(0)))));
+            this.diagramShape14.Content = "12";
+            this.diagramShape14.ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            this.diagramShape14.Position = new DevExpress.Utils.PointFloat(160F, 192F);
+            this.diagramShape14.Size = new System.Drawing.SizeF(80F, 45F);
+            this.diagramShape14.StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
             // 
             // Form1
             // 
@@ -149,12 +222,19 @@
         private DevExpress.XtraDiagram.DiagramControl diagramControl1;
         private DevExpress.XtraDiagram.DiagramShape diagramShape1;
         private DevExpress.XtraDiagram.DiagramShape diagramShape2;
+        private DevExpress.XtraDiagram.DiagramContainer diagramContainer1;
         private DevExpress.XtraDiagram.DiagramShape diagramShape3;
         private DevExpress.XtraDiagram.DiagramShape diagramShape4;
         private DevExpress.XtraDiagram.DiagramShape diagramShape5;
         private DevExpress.XtraDiagram.DiagramShape diagramShape6;
         private DevExpress.XtraDiagram.DiagramShape diagramShape7;
         private DevExpress.XtraDiagram.DiagramShape diagramShape8;
+        private DevExpress.XtraDiagram.DiagramShape diagramShape9;
+        private DevExpress.XtraDiagram.DiagramShape diagramShape10;
+        private DevExpress.XtraDiagram.DiagramShape diagramShape11;
+        private DevExpress.XtraDiagram.DiagramShape diagramShape12;
+        private DevExpress.XtraDiagram.DiagramShape diagramShape13;
+        private DevExpress.XtraDiagram.DiagramShape diagramShape14;
     }
 }
 
--- WindowTestTask/Form1.cs
+++ WindowTestTask/Form1.cs
@@ -17,4 +17,164 @@ namespace WindowTestTask
             InitializeComponent();
         }
     }
+
+    abstract class BasicNonContainerView : DevExpress.XtraDiagram.DiagramShape
+    {
+        protected BasicNonContainerView()
+        {
+        }
+
+
+        protected BasicNonContainerView(string textOnDisplay, string stateOfShape, float locationX, float locationY)
+        {
+            // зависимые от входных данных
+            SetColorView(stateOfShape);
+            Position = new DevExpress.Utils.PointFloat(locationX, locationY);
+            Content = textOnDisplay;
+            //постоянные для всех потомков
+            ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+            Size = new System.Drawing.SizeF(80F, 50F);
+            StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+        }
+
+        protected virtual void SetColorView(string color)
+        {
+            switch (color)
+            {
+                case "Warning":
+                    Appearance.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (255)))),
+                        ((int) (((byte) (255)))),
+                        ((int) (((byte) (0)))));
+                    break;
+                case "Alarm":
+                    Appearance.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (255)))),
+                        ((int) (((byte) (0)))),
+                        ((int) (((byte) (0)))));
+                    break;
+
+                default:
+                    Appearance.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (255)))),
+                        ((int) (((byte) (255)))),
+                        ((int) (((byte) (255)))));
+                    break;
+            }
+        }
+    }
+
+     class ViewSensor : BasicNonContainerView
+    {
+        internal ViewSensor(string textOnDisplay, string stateOfShape, float locationX, float locationY)
+        :base(textOnDisplay,  stateOfShape,  locationX,  locationY)
+        {
+            Shape= DevExpress.Diagram.Core.BasicShapes.RoundedRectangle;
+        }
+
+        protected override void SetColorView(string color)
+        {
+            switch (color)
+            {
+                case "GoodOrOff":
+                    Appearance.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))),
+                        ((int) (((byte) (150)))),
+                        ((int) (((byte) (0)))));
+                    break;
+                default:
+                    base.SetColorView(color);
+                    break;
+            }
+        }
+
+    }
+
+     class ViewHeatingLine : BasicNonContainerView
+     {
+         internal ViewHeatingLine(string textOnDisplay, string stateOfShape, float locationX, float locationY)
+             : base(textOnDisplay, stateOfShape, locationX, locationY)
+         {
+         }
+
+         protected override void SetColorView(string color)
+         {
+             switch (color)
+             {
+                 case "GoodOrOff":
+                     Appearance.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (80)))),
+                         ((int) (((byte) (80)))),
+                         ((int) (((byte) (80)))));
+                     break;
+                 default:
+                     base.SetColorView(color);
+                     break;
+             }
+         }
+     }
+
+     class ViewAlarmOnContainer : BasicNonContainerView
+     {
+         ViewAlarmOnContainer(float locationX, float locationY)
+             : base("Авария", "Alarm", locationX, locationY)
+         {
+             
+         }
+     }
+
+     class ViewContainerStatus : DevExpress.XtraDiagram.DiagramShape
+     {
+         internal ViewContainerStatus(string status, float locationX, float locationY)
+         {
+             // меняется для индикаторов статуса
+             SetColorView(status);
+             Position = new DevExpress.Utils.PointFloat(locationX, locationY);
+             // стандартные для всех индикаторов статуса
+             Shape = DevExpress.Diagram.Core.BasicShapes.Ellipse;
+             Size = new System.Drawing.SizeF(15F, 15F);
+             StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+        }
+
+         protected virtual void SetColorView(string color)
+         {
+             switch (color)
+             {
+                 case "-1":
+                     Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))),
+                         ((int)(((byte)(80)))),
+                         ((int)(((byte)(80)))));
+                     break;
+                 case "0":
+                    Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))),
+                        ((int)(((byte)(0)))),
+                        ((int)(((byte)(0)))));
+                    break;
+                case "1":
+                     Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))),
+                         ((int)(((byte)(150)))),
+                         ((int)(((byte)(0)))));
+                     break;
+                default:
+                    Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))),
+                        ((int)(((byte)(255)))),
+                        ((int)(((byte)(255)))));
+                    break;
+            }
+        }
+     }
+
+     class ViewContainerText : DevExpress.XtraDiagram.DiagramShape
+     {
+         private float elementSizeWidth = 100F;
+         private float elementSizeHeight = 15F;
+
+         internal ViewContainerText(string text, string status, float locationX, float locationY)
+         {
+             // меняется для текста контейнера
+             Content = text;
+             Position = new DevExpress.Utils.PointFloat(locationX, locationY);
+             // Постоянные текста в контейнере
+             ForegroundId = DevExpress.Diagram.Core.DiagramThemeColorId.Black;
+             Size = new System.Drawing.SizeF(elementSizeWidth, elementSizeHeight);
+             StrokeId = DevExpress.Diagram.Core.DiagramThemeColorId.Accent1;
+            // добавить индикатор статуса
+            ViewContainerStatus statusText = new ViewContainerStatus(status, elementSizeWidth + locationX, locationY);
+         }
+     }
 }
